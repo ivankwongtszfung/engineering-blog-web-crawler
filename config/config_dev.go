9db67f93b84ceafc8b5efadbc9d24cd076acd674
Ivan Kwong <ivankwongtszfung@gmail.com>
2024-06-19 14:42:08 -0400

    feat: rename the db_host

--- config/config_dev.go
+++ config/config_dev.go
@@ -4,7 +4,7 @@ package config
 
 const (
 	REDIS_DATABASE = "localhost:6379"
-	DB_HOST        = "./db/test.db"
+	DB_HOST        = "./db/dev.db"
 	DB_DRIVER      = "sqlite3"
 	API_PORT       = 8080
 )
