cba62a6568b39a1a6d1cc28aa316e4704348db1e
Ivan Kwong <ivankwongtszfung@gmail.com>
2024-06-19 14:45:33 -0400

    chore: add comments, replace fmt to log

--- cmd/main.go
+++ cmd/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
 	"log"
 	"strings"
 	"sync"
@@ -14,8 +13,10 @@ import (
 	"github.com/ivankwongtszfung/engineering-blog-web-crawler/infra/repo"
 	"github.com/ivankwongtszfung/engineering-blog-web-crawler/infra/repo/sqlite"
 	"github.com/ivankwongtszfung/engineering-blog-web-crawler/pkg/kvstore"
+	"github.com/pkg/errors"
 
 	"github.com/gocolly/colly"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 const ENGINEERING_PAGE_PREFIX string = "https://www.uber.com/en-CA/blog/engineering/page/"
@@ -42,7 +43,7 @@ func scrapeUberBlogs(kv kvstore.KVStore, articles chan<- *blog.Article) {
 		if className == "flex-grid-item" {
 			article, err := uber.ExtractArticle(e)
 			if err != nil {
-				fmt.Printf("Error extracting article: %v\n", err)
+				log.Print(errors.Wrap(err, "Error extracting article"))
 				return
 			}
 			if val, _ := kv.Exist(article.BlogURL); val {
@@ -64,34 +65,38 @@ func scrapeUberBlogs(kv kvstore.KVStore, articles chan<- *blog.Article) {
 		if val, _ := kv.Exist(link); val {
 			return
 		}
-		fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+		log.Printf("Link found: %q -> %s\n", e.Text, link)
 		kv.Set(e.Request.AbsoluteURL(link), "")
 		// c.Visit(e.Request.AbsoluteURL(link))
 	})
 
 	// Set error handler
 	c.OnError(func(r *colly.Response, err error) {
-		fmt.Println("Request URL:", r.Request.URL, "failed with response:", r, "\nError:", err)
+		log.Println("Request URL:", r.Request.URL, "failed with response:", r, "\nError:", err)
 	})
 
 	c.Visit("https://www.uber.com/en-CA/blog/engineering/")
 	c.Wait()
-	fmt.Print("close the article")
 }
 
 func main() {
 
-	kv := kvstore.NewRedisStore(config.REDIS_DATABASE)
+	// kv store init
+	var kv kvstore.KVStore = kvstore.NewRedisStore(config.REDIS_DATABASE)
 	if err := kv.Ping(); err != nil {
 		log.Fatalln("failed to connect to redis", err)
 	}
+	defer kv.Close()
 
+	// article channel init
 	articles := make(chan *blog.Article)
 	go func() {
+		log.Print("close the article")
 		defer close(articles)
 		scrapeUberBlogs(kv, articles)
 	}()
 
+	// db init
 	db, err := sql.Open(config.DB_DRIVER, config.DB_HOST)
 	if err != nil {
 		log.Fatalln(err)
@@ -102,16 +107,21 @@ func main() {
 	if err != nil {
 		log.Panic(err)
 	}
-
 	var articleRepo repo.IArticleRepository = sqlite.ArticleRepository{DB: db}
+	articleRepo.Create()
 
+	// persist in db
 	var wg sync.WaitGroup
 	for range 5 {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
 			for article := range articles {
-				articleRepo.SaveOne(*article)
+				err = articleRepo.SaveOne(*article)
+				if err != nil {
+					log.Print(err)
+					kv.Delete(article.BlogURL)
+				}
 			}
 		}()
 	}
